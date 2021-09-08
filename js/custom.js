52c369b63e525a95b3cb88e268aedc7fdcb0e523
Volodimir Ilinskiy <creavit@i.ua>
2021-09-08 23:08:15 +0300

    Parameter Value Validation

--- js/custom.js
+++ js/custom.js
@@ -1,57 +1,63 @@
 function decomposeIntoFactors(num){
 	/*
-	* Функция возвращает объект содержащий число и два массива с множителями и их степенями
+	* Функция возвращает объект 
+  * https://github.com/creavit19/decompose-into-factors
   * https://creavit19.github.io/decompose-into-factors/
 	*/
-	let answer = {
-		number : 1,
-		factor : [],
-		degree : []
-	}
-	if(num == 1){answer.factor[0] = 1; answer.degree[0] = 1; return answer};
+  
+  let answer = [];
+  answer.number = num;
+  answer.error = false;
 
-	answer.number = num;
+  if (typeof num != 'number' || num < 0) { answer.error = true; return answer }
+	
+  if (num == 0) { answer.push({factor: 0, degree: 1}); return answer };
+	if (num == 1) { answer.push({factor: 1, degree: 1}); return answer };
+
 	let remain = num;
 	let sqrtRemain = sqRound(remain);
 	let currentNum = 1;
   let primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19];
 
   for (currentNum of primeNumbers) {
-    if(remain % currentNum == 0) if(recInAnswer()) return answer;
+    if (remain % currentNum == 0) if (recInAnswer()) return answer;
   }
 	
   do {
 		
-    currentNum += 2;
+    currentNum++;
+    currentNum++;
     
-    if(currentNum % 3 == 0) continue;
-    if(currentNum % 5 == 0) continue;
-    if(currentNum % 7 == 0) continue;
-    if(currentNum % 11 == 0) continue;
-    if(currentNum % 13 == 0) continue;
-    if(currentNum % 17 == 0) continue;
-    if(currentNum % 19 == 0) continue;
+    if (currentNum % 3 == 0) continue;
+    if (currentNum % 5 == 0) continue;
+    if (currentNum % 7 == 0) continue;
+    if (currentNum % 11 == 0) continue;
+    if (currentNum % 13 == 0) continue;
+    if (currentNum % 17 == 0) continue;
+    if (currentNum % 19 == 0) continue;
 
-		if(remain % currentNum != 0) continue;
-		if(recInAnswer()) return answer;
+		if (remain % currentNum != 0) continue;
+		if (recInAnswer()) return answer;
 
-	} while(sqrtRemain >= currentNum);
+	} while (sqrtRemain >= currentNum);
 
-	answer.factor.push(remain);
-	answer.degree.push(1);
+  answer.push({factor: remain, degree: 1});
 	return answer;
 
 	function recInAnswer(){
-		let degree = 0;
-		do{
+		
+    let degree = 0;
+		do {
 			remain /= currentNum;
 			degree++;
-		}while(remain % currentNum == 0);
-		answer.factor.push(currentNum);
-		answer.degree.push(degree);
-		if(remain == 1) return true;
+		} while (remain % currentNum == 0);
+
+    answer.push({factor: currentNum, degree: degree});
+
+		if (remain == 1) return true;
 		sqrtRemain = sqRound(remain);
 		return false;
+
 	}
 
 	function sqRound(x){
@@ -78,12 +84,12 @@ btn.onclick=function(){
 	}
 	let ans = decomposeIntoFactors(ante);
 	let htmMes = ans.number;
-	if(ans.number == ans.factor[0]){
+	if(ans.number == ans[0].factor){
 		htmMes += ' - это простое число.';
 	}else{
 		htmMes += ' =';
-		for(let i = 0; i < ans.factor.length; i++){
-			htmMes += (i > 0 ? ' × ': ' ') + ans.factor[i] + (ans.degree[i] > 1 ? '<sup>' + ans.degree[i] + '</sup>' : '')
+		for(let i = 0; i < ans.length; i++){
+			htmMes += (i > 0 ? ' × ': ' ') + ans[i].factor + (ans[i].degree > 1 ? '<sup>' + ans[i].degree + '</sup>' : '')
 		}
 	}
 	out.innerHTML = htmMes;
